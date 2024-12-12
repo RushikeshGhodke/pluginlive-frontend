30a38f9e6e9615f1df5efa9c390b2ff0a2266f42
RushikeshGhodke <88616105+RushikeshGhodke@users.noreply.github.com>
2024-12-12 23:28:39 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "https://pluginlive-backend.onrender.com/api/v1",
-    baseURL: "http://localhost:3001/api/v1",
+    baseURL: "https://pluginlive-backend.onrender.com/api/v1",
+    // baseURL: "http://localhost:3001/api/v1",
     headers: {
         "Content-Type": "application/json",
     }
